303652baef10d7cf76f0a97f876279af526d2db8
Felipe <fsolanet5811@gmail.com>
2020-11-03 13:05:23 -0500

    record test

---
+++ src/record_test.cpp
@@ -0,0 +1,31 @@
+#include "imaging.hpp"
+
+using namespace tsw::imaging;
+
+int main()
+{
+    FlirCamera camera;
+    string serial = "20386745";
+    cout << "Connecting to camera " << serial << endl;
+    camera.Connect(serial);
+    cout << "Connected" << endl;
+
+    cout << "Creating recorder" << endl;
+
+    Recorder r(camera);
+
+    cout << "Starting record" << endl;
+    r.StartRecording("test.avi");
+
+    cout << "Starting live feed" << endl;
+    camera.StartLiveFeed();
+
+    cout << "Waiting 3 seconds" << endl;
+    sleep(3);
+
+    cout << "Stopping live feed" << endl;
+    camera.StopLiveFeed();
+
+    cout << "Stopping recording" << endl;
+    r.StopRecording();
+}
